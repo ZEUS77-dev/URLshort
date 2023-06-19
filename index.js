c2cdcc9acd4bc19b980a23b9546f03d9c021cffd
Devadutta Hati <71880311+ZEUS77-dev@users.noreply.github.com>
2023-06-19 16:59:55 +0530

    second changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -5,11 +6,11 @@ const PORT = process.env.PORT|| 8002;
 const mongoose = require('mongoose');
 const URL = require('./models/url');
 const urlRoute= require('./routes/url');
+const searchRoute = require('./routes/searchroute');
 
-mongoose.connect('mongodb+srv://devhati83:13122002@clusterdev.v2kuhlj.mongodb.net/?retryWrites=true&w=majority').then(()=>{
-    console.log('db connected');}).catch((error)=>{
-        console.log(error);
-    })
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then((e) => console.log('MongoDB connected'));
 
 
 app.set('view engine','ejs');
@@ -21,24 +22,25 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());  // or can use urlencoded 
 //express routes
-app.get('/home', async (req,res)=>{
-    const allurls=await URL.find({});
-    return res.render('home.ejs',{
-        urls : allurls,
-    });
+
+
+app.get('/',(req,res)=>{
+    return res.redirect('home');
+    //setting deafault page to home 
 });
+app.get('/home',(req,res)=>{
+    // const allurls=await URL.find({});
+    return res.render('home.ejs');
+});
+
+
 app.use('/url',urlRoute);
 
-app.get('/search/:shortid',async (req,res)=>{
-    const shortid = req.params.shortid;
-    const data = await URL.find({
-        "$or":[
-            {'note':{$regex:req.params.shortid}},
-            {'redirectURL':{$regex:req.params.shortid}},
-            {'shortid':{$regex:req.params.shortid}},
-        ]
+app.get('/archive', async (req,res)=>{
+    const allurls=await URL.find({});
+    return res.render('archivepage.ejs',{
+        data : allurls,
     })
-    res.send(data);
 });
 
 
@@ -59,11 +61,23 @@ app.get('/url/:shortid',async (req,res)=>{
     res.redirect(entry.redirectURL);
 })
 
+app.post('/search',async (req,res)=>{
+    const body= req.body.input;
+    const data = await URL.find({
+        "$or":[
+            {'note':{$regex:body}},
+            {'redirectURL':{$regex:body}},
+            {'shortid':{$regex:body}},
+        ]
+    })
+    return res.render('searchpage.ejs',{
+        packet : data,
+    });
+});
 
-
-
-
-
+app.use('/search',(req,res)=>{
+    return res.render('searchpage');
+});
 
 app.listen(PORT || process.env.PORT,()=>console.log((`Server started at ${PORT}`)));
 
---
+++ routes/searchroute.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const URL = require('../models/url');
+const { handleError } = require('parse/lib/browser/RESTController');
+const router = express.Router();
+
+
+
+
+
+module.exports = router;
